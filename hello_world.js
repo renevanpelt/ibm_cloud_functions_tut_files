e5f3d502d07ade5c8df5f205c50c5a5cdb69601a
Your Name <you@example.com>
2018-12-02 18:44:17 +0100

    update to use argument

--- hello_world.js
+++ hello_world.js
@@ -2,6 +2,7 @@
  * IBM Bluemix Cloud Action Name: hello_world
  */
 
-function main() {
-   return {payload: 'Hello world'};
+function main(params) {
+  const name = params.name || "world";
+  return {payload: 'Hello ' + name};
 }
